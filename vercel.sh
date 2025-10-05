fd1740738d36de94456d0e51a81d6dda169824fb
Matthias Gutsch <matthias.gutsch@gmail.com>
2025-10-05 19:30:34 +0200

    ok

--- vercel.sh
+++ vercel.sh
@@ -1,3 +1,4 @@
+
 if [[ $VERCEL_GIT_COMMIT_REF == "main" ]]; then
   echo "This is our production branch"
   npm run build:ssr
@@ -7,4 +8,4 @@ elif [[ $VERCEL_GIT_COMMIT_REF == "master" ]]; then
 else
   echo "This is not our production branch"
   npm run build:staging-ssr
-fi 
+fi
