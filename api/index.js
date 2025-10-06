7f9ef284b131d28883ad12819b78739d31a8a831
Matthias Gutsch <matthias.gutsch@gmail.com>
2025-10-06 18:48:18 +0200

    ok

--- api/index.js
+++ api/index.js
@@ -1,2 +1,4 @@
-import server from '../dist/studionexo_20/server/server.mjs';
-export default server;
+export default async function handler(req, res) {
+  const { default: server } = await import('../dist/studionexo_20/server/server.mjs');
+  return server(req, res);
+}
