b1ffb5a5efdc5bb0df0850917658d6d9b9b569c1
Matthias Gutsch <matthias.gutsch@gmail.com>
2025-10-06 09:12:50 +0200

    ok

--- api/index.js
+++ api/index.js
@@ -1,5 +1,4 @@
 export default async function handler(req, res) {
   const { app } = await import('../dist/studionexo_20/server/main.server.mjs');
-  const handle = app();
-  return handle(req, res);
+  return app()(req, res);
 }
